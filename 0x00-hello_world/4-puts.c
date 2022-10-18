ac78d65f754bfe666e1abcd341de5522ead6f649
root <eshedania>
2022-10-18 08:55:26 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * print string
+ *
+ *
+ *return 0*/
+
+int main(void){
+
+	char name[51] = "Programming is like building a multilingual puzzle";
+	
+
+	puts(name);
+
+	return 0;
+
+}
